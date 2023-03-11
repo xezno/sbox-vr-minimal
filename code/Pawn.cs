04ef11abb0a53eb8904a598af0da1268b2ec5bef
Alex <alex@gu3.me>
2023-03-11 00:31:46 +0000

    Initial Commit

---
+++ code/Pawn.cs
@@ -0,0 +1,157 @@
+﻿using Sandbox;
+using System.ComponentModel;
+
+namespace MyGame;
+
+public partial class Pawn : AnimatedEntity
+{
+	[ClientInput]
+	public Vector3 InputDirection { get; set; }
+
+	[ClientInput]
+	public Angles ViewAngles { get; set; }
+
+	/// <summary>
+	/// Position a player should be looking from in world space.
+	/// </summary>
+	[Browsable( false )]
+	public Vector3 EyePosition
+	{
+		get => Transform.PointToWorld( EyeLocalPosition );
+		set => EyeLocalPosition = Transform.PointToLocal( value );
+	}
+
+	/// <summary>
+	/// Position a player should be looking from in local to the entity coordinates.
+	/// </summary>
+	[Net, Predicted, Browsable( false )]
+	public Vector3 EyeLocalPosition { get; set; }
+
+	/// <summary>
+	/// Rotation of the entity's "eyes", i.e. rotation for the camera when this entity is used as the view entity.
+	/// </summary>
+	[Browsable( false )]
+	public Rotation EyeRotation
+	{
+		get => Transform.RotationToWorld( EyeLocalRotation );
+		set => EyeLocalRotation = Transform.RotationToLocal( value );
+	}
+
+	/// <summary>
+	/// Rotation of the entity's "eyes", i.e. rotation for the camera when this entity is used as the view entity. In local to the entity coordinates.
+	/// </summary>
+	[Net, Predicted, Browsable( false )]
+	public Rotation EyeLocalRotation { get; set; }
+
+	public BBox Hull
+	{
+		get => new
+		(
+			new Vector3( -16, -16, 0 ),
+			new Vector3( 16, 16, 64 )
+		);
+	}
+
+	[BindComponent] public PawnController Controller { get; }
+	[BindComponent] public PawnAnimator Animator { get; }
+
+	public override Ray AimRay => new Ray( EyePosition, EyeRotation.Forward );
+
+	/// <summary>
+	/// Called when the entity is first created 
+	/// </summary>
+	public override void Spawn()
+	{
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+	}
+
+	public void Respawn()
+	{
+		Components.Create<PawnController>();
+		Components.Create<PawnAnimator>();
+	}
+
+	public void DressFromClient( IClient cl )
+	{
+		var c = new ClothingContainer();
+		c.LoadFromClient( cl );
+		c.DressEntity( this );
+	}
+
+	public override void Simulate( IClient cl )
+	{
+		SimulateRotation();
+		Controller?.Simulate( cl );
+		Animator?.Simulate();
+		EyeLocalPosition = Vector3.Up * (64f * Scale);
+	}
+
+	public override void BuildInput()
+	{
+		var analogValue = Input.VR.LeftHand.Joystick.Value;
+		var headAngles = Input.VR.Head.Rotation.Angles();
+
+		var moveRotation = Rotation.From( 0, headAngles.yaw, 0 );
+		InputDirection = new Vector3( -analogValue.y, analogValue.x, 0 ) * moveRotation;
+
+		DebugOverlay.Line( Position, Position + InputDirection * 128f, 0, false );
+
+		if ( Input.StopProcessing )
+			return;
+
+		var look = Angles.Zero;
+
+		if ( ViewAngles.pitch > 90f || ViewAngles.pitch < -90f )
+		{
+			look = look.WithYaw( look.yaw * -1f );
+		}
+
+		var viewAngles = ViewAngles;
+		viewAngles += look;
+		viewAngles.pitch = viewAngles.pitch.Clamp( -89f, 89f );
+		viewAngles.roll = 0f;
+		ViewAngles = viewAngles.Normal;
+	}
+
+	public override void FrameSimulate( IClient cl )
+	{
+		SimulateRotation();
+
+		Camera.Rotation = ViewAngles.ToRotation();
+		Camera.Position = EyePosition;
+		Camera.FieldOfView = Screen.CreateVerticalFieldOfView( Game.Preferences.FieldOfView );
+		Camera.FirstPersonViewer = this;
+	}
+
+	public TraceResult TraceBBox( Vector3 start, Vector3 end, float liftFeet = 0.0f )
+	{
+		return TraceBBox( start, end, Hull.Mins, Hull.Maxs, liftFeet );
+	}
+
+	public TraceResult TraceBBox( Vector3 start, Vector3 end, Vector3 mins, Vector3 maxs, float liftFeet = 0.0f )
+	{
+		if ( liftFeet > 0 )
+		{
+			start += Vector3.Up * liftFeet;
+			maxs = maxs.WithZ( maxs.z - liftFeet );
+		}
+
+		var tr = Trace.Ray( start, end )
+					.Size( mins, maxs )
+					.WithAnyTags( "solid", "playerclip", "passbullets" )
+					.Ignore( this )
+					.Run();
+
+		return tr;
+	}
+
+	protected void SimulateRotation()
+	{
+		EyeRotation = ViewAngles.ToRotation();
+		Rotation = ViewAngles.WithPitch( 0f ).ToRotation();
+	}
+}
